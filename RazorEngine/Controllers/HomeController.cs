12c7190f18b124f4ee73a88b77081a7b73342b57
Enrique Cerviño <enrique.cervino@ALTIA.VIGO>
2019-10-09 13:09:28 +0200

    First commit

---
+++ RazorEngine/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RazorEngine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorEngine.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Product myProduct = new Product
+            {
+                ProductID = 1,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Category = "Watersports",
+                Price = 275M
+            };
+            ViewBag.StockLevel = 2;//THIS PROPERTY IS DEFINED HERE, COMO "VIEWBAG" ES DINAMICO NO TENGO QUE DECLARAR NADA
+            return View(myProduct);
+        }
+    }
+
+}
+
